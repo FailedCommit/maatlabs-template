31b165b8a27f11d0e43917c3bad3eee732838929
Mohit Kumar <27889519+FailedCommit@users.noreply.github.com>
2022-12-09 10:56:08 +0530

    Adds the gradle plugin to auto generate Java classes from openAPI spec

--- api/src/main/java/com/maatlabs/application/DemoApplication.java
+++ api/src/main/java/com/maatlabs/application/DemoApplication.java
@@ -1,25 +1,12 @@
 package com.maatlabs.application;
 
-import com.maatlabs.service.MyService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication(scanBasePackages = "com.maatlabs")
-@RestController
+@SpringBootApplication
 public class DemoApplication {
-
-	private final MyService myService;
-
-	public DemoApplication(MyService myService) {
-		this.myService = myService;
-	}
-
-	@GetMapping("/")
-	public String home() {
-		return myService.message();
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
---
+++ services/src/main/java/com/maatlabs/service/ArtistsController.java
@@ -0,0 +1,4 @@
+package com.maatlabs.service;
+
+public class ArtistsController {
+}
